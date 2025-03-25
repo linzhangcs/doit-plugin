7e4355af09049a618ef3a664583fd25e846cdc46
Lin Zhang <lin.zhang.ux@gmail.com>
2025-03-25 16:49:19 -0700

    chore: adding tracker component to the view to test

--- src/views/daily-view.jsx
+++ src/views/daily-view.jsx
@@ -1,18 +1,35 @@
 import TodoSystem from "../components/daily/todo-system";
+// import DailyTrackers from "../components/daily/daily-trackers";
+import Tracker from "../components/tracker";
+import { FaTint } from "react-icons/fa"; // Water drop icon
+import { FaSmile, FaMeh, FaFrown } from "react-icons/fa"; // Emojis
+import styles from "../styles/components/daily-view.module.scss";
 import DailyTrackers from "../components/daily/daily-trackers";
 
-import styles from "../styles/components/daily-view.module.scss";
+const DailyView = () => {
+  // const stressLevels = [
+  //   { id: 1, icon: <FaSmile className="text-yellow-400" />, label: "Happy" },
+  //   { id: 2, icon: <FaSmile />, label: "Content" },
+  //   { id: 3, icon: <FaMeh />, label: "Neutral" },
+  //   { id: 4, icon: <FaFrown />, label: "Sad" },
+  //   { id: 5, icon: <FaFrown className="text-gray-500" />, label: "Very Sad" },
+  // ];
 
-const DailyView = () => (
-  <section>
-    <div className={styles.sectionTab}>
-      today: {new Date().toLocaleDateString()}
-    </div>
-    <div className={styles.sectionCanvas}>
-      <TodoSystem />
-      <DailyTrackers />
-    </div>
-  </section>
-);
+  const waterIcon = [{ icon: <FaTint /> }];
+
+  return (
+    <section>
+      <div className={styles.sectionTab}>
+        today: {new Date().toLocaleDateString()}
+      </div>
+      <div className={styles.sectionCanvas}>
+        <TodoSystem />
+        <DailyTrackers />
+        <Tracker title="Water Tracker" maxCount={8} icons={waterIcon} />
+        <Tracker title="Workout Tracker" maxCount={7} />
+      </div>
+    </section>
+  );
+};
 
 export default DailyView;
