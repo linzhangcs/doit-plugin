f3f1d1d4a1093a1cee5cf4d56f06e695a327259c
Lin Zhang <lin.zhang.ux@gmail.com>
2025-03-25 23:27:41 -0700

    chore: move the tracker components out of the view

--- src/views/daily-view.jsx
+++ src/views/daily-view.jsx
@@ -1,22 +1,9 @@
 import TodoSystem from "../components/daily/todo-system";
-// import DailyTrackers from "../components/daily/daily-trackers";
-import Tracker from "../components/tracker";
-import { FaTint } from "react-icons/fa"; // Water drop icon
-import { FaSmile, FaMeh, FaFrown } from "react-icons/fa"; // Emojis
-import styles from "../styles/components/daily-view.module.scss";
 import DailyTrackers from "../components/daily/daily-trackers";
 
+import styles from "../styles/components/daily-view.module.scss";
+
 const DailyView = () => {
-  // const stressLevels = [
-  //   { id: 1, icon: <FaSmile className="text-yellow-400" />, label: "Happy" },
-  //   { id: 2, icon: <FaSmile />, label: "Content" },
-  //   { id: 3, icon: <FaMeh />, label: "Neutral" },
-  //   { id: 4, icon: <FaFrown />, label: "Sad" },
-  //   { id: 5, icon: <FaFrown className="text-gray-500" />, label: "Very Sad" },
-  // ];
-
-  const waterIcon = [{ icon: <FaTint /> }];
-
   return (
     <section>
       <div className={styles.sectionTab}>
@@ -25,8 +12,6 @@ const DailyView = () => {
       <div className={styles.sectionCanvas}>
         <TodoSystem />
         <DailyTrackers />
-        <Tracker title="Water Tracker" maxCount={8} icons={waterIcon} />
-        <Tracker title="Workout Tracker" maxCount={7} />
       </div>
     </section>
   );
